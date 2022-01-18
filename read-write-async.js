44bbeb4c50aca1183f34c342e575209f6867ccd8
KingKellee <kellyiyogun@gmail.com>
2022-01-19 00:01:56 +0100

    Learnt asynchronous approach to read and write file

---
+++ read-write-async.js
@@ -0,0 +1,38 @@
+// How to read and write file-Asynchronous Approach
+
+const { clear } = require('console')
+const {readFile, writeFile, read} = require('fs')
+
+// the readFile takes in as argument a path and a call back funnction
+// the callback function takes an error, and the result as parameters
+
+readFile('./content/first.txt', 'utf8', (err, result)=>{
+    if (err) {
+        console.log(err)
+        return
+    }
+    const first = result
+    
+    readFile('./content/second.txt', 'utf8', (err, result)=>{
+        if (err){
+            console.log(err)
+            return
+        }
+        const second = result
+        console.log(second)
+
+        writeFile('./content/result-async.txt', 
+        `This is the result of writeFileSync: ${first} and ${second}`,
+        (err, result)=>{
+            if (err){
+                console.log(err)
+                return 
+            }
+            console.log (result)
+        }
+        )
+    })
+
+
+})
+
