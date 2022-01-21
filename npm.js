84315d56bc82581ce6cc3ac8e6cc66680ce300ce
KingKellee <kellyiyogun@gmail.com>
2022-01-21 23:05:08 +0100

    Learnt about npm, installing packages and nodemo

---
+++ npm.js
@@ -0,0 +1,20 @@
+// npm - global command, comes installed with node
+//  to check npm version npm --version
+
+//  local dependency - use it only in this particular project
+//  npm i <packageName>
+
+// global dependenc - use it in any project
+//  npm install -g <packageName>
+// sud install -g <packageName> (mac)
+
+// package.json - manifest file (it stores important information about project/package)
+// manual approach is to create a package.json in the root of your project dir, ctreate properties etc
+// or you run npm init (step by step, press enter to skip)
+//  npm init -y (everything default)
+
+
+// DEV DEPENDENCIES 
+// Use npm i <packageName> -D OR --save-dev
+// we add dependicies that are only neccessary when developing the apps but not really needed for production
+// example include nodemo, lynting, testing-packages
