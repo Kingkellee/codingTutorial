60f54baa52404a9eec9ebabb6ef78d2dcd3a8493
KingKellee <kellyiyogun@gmail.com>
2022-01-28 12:20:30 +0100

    Getting started with express

---
+++ express-tutorial/final/getting-started.js
@@ -0,0 +1,51 @@
+// Getting Started with Node-Express Tutorial 
+
+
+const http = require('http')
+
+// 
+const {readFileSync} = require('fs')
+
+const homePage = readFileSync('.//navbar-app/index.html')
+const homeStyles = readFileSync('./navbar-app/styles.css')
+const homeImage = readFileSync('./navbar-app/logo.svg')
+const homeLogic = readFileSync('./navbar-app/browser-app.js')
+
+const server = http.createServer((req, res)=>{
+
+    const url = req.url
+
+    // Homepage
+    if(url === "/") {
+        res.writeHead(200, {'content-type': 'text/html'})
+        res.write(homePage)
+        res.end()
+    } 
+    // About Page
+    else if (url === "/about") {
+        res.end("About")
+    } 
+    else if (url === "/styles.css") {
+        res.writeHead(200, {'content-type': 'text/css'})
+        res.write(homeStyles)
+        res.end()
+    }
+    else if (url === "/logo.svg") {
+        res.writeHead(200, {'content-type': 'image/svg+xml'})
+        res.write(homeImage)
+        res.end()
+    }
+    else if (url === "/browser-app.js") {
+        res.writeHead(200, {'content-type': 'text/javascript'})
+        res.write(homeLogic)
+        res.end()
+    }
+    // Page not found
+    else {
+        res.writeHead(404, {'content-type': 'text/html'})
+        res.write("<h1>OOPS!! we were unable to find the resource you are searching for</h1>")
+        res.end()
+    }
+})
+
+server.listen(5000, ()=>{ console.log("server now listening on port 5000...")})
